006601b4891d644c311aa193de45acdbaf839c98
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-19 15:49:54 +0200

    test

--- schnitzelstart.py
+++ schnitzelstart.py
@@ -580,7 +580,7 @@ def location(bot, update):
                                 keyboard.append([InlineKeyboardButton(str(is_right), callback_data="question;;"+str(is_right)+";;"+str(chatid)+";;"+"true;;"+str(long)+";;"+str(lat))])
                                 keyboard.append([InlineKeyboardButton(str(is_wrong), callback_data="question;;"+str(is_wrong)+";;"+str(chatid)+";;"+"false;;"+str(long)+";;"+str(lat))])
                             else:
-                                mydata = "question;;"+str(is_wrong)+";;"+str(chatid)+";;"+"false;;"+str(long)+";;"+str(lat)
+                                mydata = "question;;"+str(is_wrong)+str(emojiarray[1])+";;"+str(chatid)+";;"+"false;;"+str(long)+";;"+str(lat)
                                 keyboard.append([InlineKeyboardButton(str(is_wrong), callback_data=mydata)])
                                 keyboard.append([InlineKeyboardButton(str(is_right), callback_data="question;;"+str(is_right)+";;"+str(chatid)+";;"+"true;;"+str(long)+";;"+str(lat))])
 
