77c3d6d0ea34272c1ecedfa5cfac8b306ad27662
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-20 17:25:41 +0200

    added git pull to start command

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 ps aux | grep schnitzelstart.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep schnitzelstart.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 /usr/bin/python schnitzelstart.py >> /tmp/schnitzel.log 2>&1 &
