de4156fc4857044924d055b82041a637516f9dd1
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-23 22:02:35 +0200

    update start file with messages

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-git pull
+echo "Updating SAM via git pull"
+echo "(this may take a while)"
+git pull > /tmp/sam-update.log 2>&1
+echo
+echo "Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+echo "Starting Bot Instance"
+
 /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 &
+echo "Killing old WebGui Instance"
 
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+echo "Staring WebGui Instance"
 /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 &
+echo "SAM has been updated and restarted"
