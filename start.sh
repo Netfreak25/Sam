a02f698713b922bce430364dbd34601f31bccf15
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-23 22:10:39 +0200

    test

--- start.sh
+++ start.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-echo "[1/5] Updating SAM via git pull"
-echo "(this may take a while)"
+echo -ne "[1/5] Updating SAM via git pull"
 git pull > /tmp/sam-update.log 2>&1
+echo -ne "\\r[1/5] Updating SAM via git pull - DONE"
 
-echo "[2/5] Killing old Bot Instance"
+
+echo -ne "[2/5] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+echo -ne "\\r[2/5] Killing old Bot Instance - DONE"
 
-echo "[3/5] Starting Bot Instance"
+echo -ne "[3/5] Starting Bot Instance"
 /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 &
+echo -ne "\\r[3/5] Starting Bot Instance - DONE"
 
-echo "[4/5] Killing old WebGui Instance"
+echo -ne "[4/5] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+echo -ne "\\r[4/5] Killing old WebGui Instance - DONE"
 
-echo "[5/5] Staring WebGui Instance"
+echo -ne "[5/5] Staring WebGui Instance"
 /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 &
+echo -ne "\\r[5/5] Staring WebGui Instance - DONE"
 
 echo
 echo "SAM has been updated and restarted!"
