760cd9da426cf8318a388b2abfbe570caba3817f
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-23 16:00:05 +0200

    update start file

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 git pull
-ps aux | grep schnitzelstart.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
-ps aux | grep schnitzelstart.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
-/usr/bin/python schnitzelstart.py >> /tmp/schnitzel.log 2>&1 &
+ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+/usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 &
+
+ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+/usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 &
