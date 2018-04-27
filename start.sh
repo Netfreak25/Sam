60852d38aacac6fa6a031a1487c9b1e6d3739bd1
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-27 12:49:08 +0200

    improvement

--- start.sh
+++ start.sh
@@ -18,10 +18,6 @@ git pull > /tmp/sam-update.log 2>&1
 echo -ne "\\r[1/5] Updating SAM via git pull - DONE"
 echo
 
-
-
-
-
 echo -ne "[2/5] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -36,10 +32,6 @@ else
 fi
 echo
 
-
-
-
-
 echo -ne "[3/5] Starting Bot Instance"
 sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 & echo $! > /tmp/sam-bot.pid
 
@@ -50,10 +42,6 @@ else
 	echo -ne "\\r[3/5] Starting Bot Instance - SUCCESS"
 fi
 echo
-
-
-
-
 
 echo -ne "[4/5] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -68,10 +56,6 @@ else
     echo -ne "\\r[4/5] Killing old WebGui Instance - FAILED"
 fi
 echo
-
-
-
-
 
 echo -ne "[5/5] Staring WebGui Instance"
 sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 & echo $! > /tmp/sam-gui.pid
