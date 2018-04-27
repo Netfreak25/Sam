c6ccaf0a40c790b1994f33624afc67e6c9de76bb
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-27 12:52:45 +0200

    ...

--- start.sh
+++ start.sh
@@ -17,7 +17,6 @@ echo -ne "[1/5] Updating SAM via git pull"
 git pull > /tmp/sam-update.log 2>&1
 echo -ne "\\r[1/5] Updating SAM via git pull - DONE"
 echo
-
 echo -ne "[2/5] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -31,7 +30,6 @@ else
     echo -ne "\\r[2/5] Killing old Bot Instance - FAILED"
 fi
 echo
-
 echo -ne "[3/5] Starting Bot Instance"
 sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 & echo $! > /tmp/sam-bot.pid
 
@@ -42,7 +40,6 @@ else
 	echo -ne "\\r[3/5] Starting Bot Instance - SUCCESS"
 fi
 echo
-
 echo -ne "[4/5] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -56,7 +53,6 @@ else
     echo -ne "\\r[4/5] Killing old WebGui Instance - FAILED"
 fi
 echo
-
 echo -ne "[5/5] Staring WebGui Instance"
 sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 & echo $! > /tmp/sam-gui.pid
 export pid=`cat /tmp/sam-gui.pid` 
