f8ef72c590fafab11896330865bec4042aebb06d
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-27 12:24:15 +0200

    saving bot pids for further checks

--- start.sh
+++ start.sh
@@ -21,22 +21,26 @@ echo
 echo -ne "[2/5] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 echo -ne "\\r[2/5] Killing old Bot Instance - DONE"
 echo
 
 echo -ne "[3/5] Starting Bot Instance"
-sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 &
+sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 & echo $! > /tmp/sam-bot.pid
 echo -ne "\\r[3/5] Starting Bot Instance - DONE"
 echo
 
 echo -ne "[4/5] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
+ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 echo -ne "\\r[4/5] Killing old WebGui Instance - DONE"
 echo
 
 echo -ne "[5/5] Staring WebGui Instance"
-sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 &
+sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 & echo $! > /tmp/sam-gui.pid
 echo -ne "\\r[5/5] Staring WebGui Instance - DONE"
 echo
 
