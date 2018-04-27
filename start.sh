923aac4847074c7a5364453c16f1bce68c6e96cd
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-27 12:38:34 +0200

    improvement for start.sh

--- start.sh
+++ start.sh
@@ -18,30 +18,69 @@ git pull > /tmp/sam-update.log 2>&1
 echo -ne "\\r[1/5] Updating SAM via git pull - DONE"
 echo
 
+
+
+
+
 echo -ne "[2/5] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
-echo -ne "\\r[2/5] Killing old Bot Instance - DONE"
+
+export pid = `cat /tmp/sam-bot.pid` 
+if ! kill $pid > /dev/null 2>&1; then
+    echo -ne "\\r[2/5] Killing old Bot Instance - SUCCESS"
+else
+    echo -ne "\\r[2/5] Killing old Bot Instance - FAILED"
+fi
 echo
+
+
+
+
 
 echo -ne "[3/5] Starting Bot Instance"
 sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 & echo $! > /tmp/sam-bot.pid
-echo -ne "\\r[3/5] Starting Bot Instance - DONE"
+
+export pid = `cat /tmp/sam-bot.pid` 
+if ! kill $pid > /dev/null 2>&1; then
+    echo -ne "\\r[3/5] Starting Bot Instance - FAILED"
+else
+	echo -ne "\\r[3/5] Starting Bot Instance - SUCCESS"
+fi
 echo
+
+
+
+
 
 echo -ne "[4/5] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
-echo -ne "\\r[4/5] Killing old WebGui Instance - DONE"
+
+export pid = `cat /tmp/sam-gui.pid` 
+if ! kill $pid > /dev/null 2>&1; then
+    echo -ne "\\r[4/5] Killing old WebGui Instance - SUCCESS"
+else
+    echo -ne "\\r[4/5] Killing old WebGui Instance - FAILED"
+fi
 echo
+
+
+
+
 
 echo -ne "[5/5] Staring WebGui Instance"
 sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 & echo $! > /tmp/sam-gui.pid
-echo -ne "\\r[5/5] Staring WebGui Instance - DONE"
+export pid = `cat /tmp/sam-gui.pid` 
+if ! kill $pid > /dev/null 2>&1; then
+    echo -ne "\\r[5/5] Staring WebGui Instance - FAILED"
+else
+    echo -ne "\\r[5/5] Staring WebGui Instance - SUCCESS"
+fi
 echo
 
 echo
