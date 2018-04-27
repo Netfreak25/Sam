4d4a58be18c2d1de2ad247c291f48ea8f79b56a4
Netfreak25 <moritz.kuhn@gmail.com>
2018-04-27 13:14:32 +0200

    cool

--- start.sh
+++ start.sh
@@ -12,12 +12,14 @@ chmod 777 /tmp/sam-update.log
 
 export username=`whoami`
 chown -R nobody:$username ./*
+if [ -f .autoupdate ]; then
+	echo -ne "[0/4] Updating SAM via git pull"
+	git pull > /tmp/sam-update.log 2>&1
+	echo -ne "\\r[0/4] Updating SAM via git pull - DONE"
+	echo
+fi
 
-echo -ne "[1/5] Updating SAM via git pull"
-git pull > /tmp/sam-update.log 2>&1
-echo -ne "\\r[1/5] Updating SAM via git pull - DONE"
-echo
-echo -ne "[2/5] Killing old Bot Instance"
+echo -ne "[1/4] Killing old Bot Instance"
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -25,22 +27,22 @@ ps aux | grep start-bot.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2
 
 export pid=`cat /tmp/sam-bot.pid`
 if ! kill -0 $pid > /dev/null 2>&1; then
-    echo -ne "\\r[2/5] Killing old Bot Instance - SUCCESS"
+    echo -ne "\\r[1/4] Killing old Bot Instance - SUCCESS"
 else
-    echo -ne "\\r[2/5] Killing old Bot Instance - FAILED"
+    echo -ne "\\r[1/4] Killing old Bot Instance - FAILED"
 fi
 echo
-echo -ne "[3/5] Starting Bot Instance"
+echo -ne "[2/4] Starting Bot Instance"
 `sudo -u nobody /usr/bin/python start-bot.py >> /tmp/sam.log 2>&1 & echo $! > /tmp/sam-bot.pid`
 
 export pid=`sleep 1 && cat /tmp/sam-bot.pid`
 if ! kill -0 $pid > /dev/null 2>&1; then
-    echo -ne "\\r[3/5] Starting Bot Instance - FAILED"
+    echo -ne "\\r[2/4] Starting Bot Instance - FAILED"
 else
-	echo -ne "\\r[3/5] Starting Bot Instance - SUCCESS"
+	echo -ne "\\r[2/4] Starting Bot Instance - SUCCESS"
 fi
 echo
-echo -ne "[4/5] Killing old WebGui Instance"
+echo -ne "[3/4] Killing old WebGui Instance"
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
 ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2>&1
@@ -48,19 +50,19 @@ ps aux | grep start-gui.py | cut -b 10-14 | xargs -I {} kill -9 {} > /dev/null 2
 
 export pid=`cat /tmp/sam-gui.pid`
 if ! kill -0 $pid > /dev/null 2>&1; then
-    echo -ne "\\r[4/5] Killing old WebGui Instance - SUCCESS"
+    echo -ne "\\r[3/4] Killing old WebGui Instance - SUCCESS"
 else
-    echo -ne "\\r[4/5] Killing old WebGui Instance - FAILED"
+    echo -ne "\\r[3/4] Killing old WebGui Instance - FAILED"
 fi
 echo
-echo -ne "[5/5] Staring WebGui Instance"
+echo -ne "[4/4] Staring WebGui Instance"
 `sudo -u nobody /usr/bin/python start-gui.py >> /tmp/sam-gui.log 2>&1 & echo $! > /tmp/sam-gui.pid`
 
 export pid=`sleep 1 && cat /tmp/sam-gui.pid`
 if ! kill -0 $pid > /dev/null 2>&1; then
-    echo -ne "\\r[5/5] Staring WebGui Instance - FAILED"
+    echo -ne "\\r[4/4] Staring WebGui Instance - FAILED"
 else
-    echo -ne "\\r[5/5] Staring WebGui Instance - SUCCESS"
+    echo -ne "\\r[4/4] Staring WebGui Instance - SUCCESS"
 fi
 echo
 
